9cf6af04f4f8d649b508c50ba859e219adee3029
Guest User <Guest@epicodus-79.local>
2019-01-15 14:20:05 -0800

    it will add new repo

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: './dist',
+        host: 'localhost',
+        port: '8081'
+    },
+    plugins: [
+        new UglifyJsPlugin({ sourceMap: true }),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Weather',
+            template: './src/index.html',
+            inject: 'body'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "eslint-loader"
+            },
+            {
+                test: /\.js$/,
+                exclude: [
+                    /node_modules/,
+                    /spec/
+                ],
+                loader: "babel-loader",
+                options: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    }
+};
